388b537c80a12cb077823891cdafcbd6bddf361b
Alexander_Ovchar <azalexxx02@gmail.com>
2024-07-31 18:50:03 +0300

    init typescript

--- src/ui-elements/PasswordLength.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-export const PasswordLength = () => {
-  const { passwordLength } = useSelector((state) => state.password);
-  return <div>Password length: {passwordLength}</div>;
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
-    fontFamily: {
-      roboto: ["Roboto", "sans-serif"],
+    extend: {
+      colors: {
+        bodyBg: 'var(--bodyBg)',
+        textColor: 'var(--textColor)',
+      },
+      boxShadow: {
+        // card: '3px 3px 5px 0 var(--shadowColor)',
+      },
     },
   },
   plugins: [],
