6d4f7e132aec1652a86fba768e4a6ac401b4dbec
Alexander_Ovchar <azalexxx02@gmail.com>
2025-06-02 20:49:36 +0300

    chore(cleanup): remove unused files and configurations

--- src/ui/PasswordLength.tsx
+++
@@ -1,6 +0,0 @@
-import {useAppSelector} from "hooks/useReduxToolkit";
-
-export const PasswordLength = () => {
-  const {passwordLength} = useAppSelector((state) => state.password);
-  return <div>Password length: {passwordLength}</div>;
-};
