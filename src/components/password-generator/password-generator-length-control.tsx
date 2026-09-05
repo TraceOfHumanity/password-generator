889d1244ccb5a29c9e591d36ef7b754ad31e3b25
TraceOfHumanity <azalexxx02@gmail.com>
2026-09-06 00:07:07 +0300

    feat(LengthControl): increase maximum password length from 64 to 128 for enhanced password generation options

--- src/components/password-generator/password-generator-length-control.tsx
+++ src/components/password-generator/password-generator-length-control.tsx
@@ -13,7 +13,7 @@ export const LengthControl = () => {
         className='h-3 w-full cursor-pointer appearance-none rounded-2xl bg-gradient-to-r from-red-500 to-green to-60% shadow-[inset_0_-1px_5px_5px_#0000002f]'
         type='range'
         min={8}
-        max={64}
+        max={128}
         value={passwordLength}
         onChange={(e) => setPasswordLength(Number(e.target.value))}
       />
