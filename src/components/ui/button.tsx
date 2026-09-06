988bf6dcb2aa08a39a2c756c07bb9ec54848549c
TraceOfHumanity <azalexxx02@gmail.com>
2026-09-06 17:26:39 +0300

    test signed commit

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,13 +5,14 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button = ({className, children}: ButtonProps) => {
+const Button = ({className, children, ...props}: ButtonProps) => {
   return (
     <button
       className={cn(
-        'border-1 border-green rounded p-2 duration-200  hover:bg-green hover:text-black disabled:opacity-50',
+        'border-1 border-green rounded p-2 duration-200  hover:bg-green hover:text-black disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-white',
         className,
-      )}>
+      )}
+      {...props}>
       {children}
     </button>
   );
